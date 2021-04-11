1b02e46c6af1241fd2980fcaa2c2dbf60955bc5c
Mike Barlow <mike@mikebarlow.co.uk>
2021-04-11 16:14:27 +0100

    Updating site to 0.3.0

---
+++ config/theme-manager.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'exceptions' => [
+        /**
+         * The view to use for ActiveThemeNotSetException
+         */
+        'activeTheme' => 'AldrumoCore::defaults.no-active-theme',
+
+        /**
+         * The view to use for ThemeNotFoundException
+         */
+        'themeNotFound' => 'AldrumoCore::defaults.theme-404',
+    ],
+];
